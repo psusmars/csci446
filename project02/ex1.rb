64b0b15a09fb5244e4dbabd0e690ce9cacc5aa7a
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-21 08:53:04 -0700

    Made it so ex1 randomly prints one of the lines, plus a return

--- project02/ex1.rb
+++ project02/ex1.rb
@@ -1,9 +1,18 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+case rand(7)
+	when 0
+		puts "Hello World!"
+	when 1
+		puts "Hello Again"
+	when 2
+		puts "I like typing this."
+	when 3
+		puts "This is fun."
+	when 4
+		puts 'Yay! Printing.'
+	when 5
+		puts "I'd much rather you 'not'."
+	when 6
+		puts 'I "said" do not touch this.'
+end
 puts
 # pound symbol for comments
