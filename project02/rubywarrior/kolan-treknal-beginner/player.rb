107b08d3446588c7eb2b4b773b945b81e0872cdf
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-22 13:18:29 -0700

    Added a rescue variable, and a method

--- project02/rubywarrior/kolan-treknal-beginner/player.rb
+++ project02/rubywarrior/kolan-treknal-beginner/player.rb
@@ -1,23 +1,38 @@
 class Player
 	@health
+	@rescued
 	def initialize
 		@health = 20
+		@rescued = false
 	end
 	
 	def play_turn(warrior)
-	if warrior.feel.empty?
-		if warrior.health < 15 and !(@health > warrior.health)
-			warrior.rest!
+	if !(@rescued)
+		if warrior.feel(:backward).empty?
+			check_health_and_heal(warrior, :backward)
 		else
-			warrior.walk!
+			warrior.rescue!(:backward)
+			@rescued = true
 		end
-	else
-		if warrior.feel.captive?
-			warrior.rescue!
+	else	
+		if warrior.feel.empty?
+			check_health_and_heal(warrior, nil)
 		else
-			warrior.attack!
+			if warrior.feel.captive?
+				warrior.rescue!
+			else
+				warrior.attack!
+			end
 		end
 	end
 	@health = warrior.health
+	end
+	
+	def check_health_and_heal(warrior, direction)
+	if warrior.health < 15 and !(@health > warrior.health)
+		warrior.rest!
+	else
+		warrior.walk!(direction)
+	end
 	end
 end
