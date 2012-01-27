e9da5ed867b7fc0971ea81da174a97ae22d15a49
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-26 20:58:20 -0700

    successful remove maybe?

--- project03/depot/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '7b524814514521ee0f58658b04209e98ecded3047906aef2ae1b7298f8d85707476237145b6f88b09a2685fc072951ad1681939e44e77895cef36cd71a9fec67'
