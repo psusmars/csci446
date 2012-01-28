cefb7abe837bae7d537295f7d8c6c27619a2e8de
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-27 21:49:54 -0700

    Added uniqueness test with I18n

--- project03/test/unit/product_test.rb
+++ project03/test/unit/product_test.rb
@@ -50,6 +50,16 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
 	
+	test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
+
+	
 	test "product is not valid without a unique title - i18n" do
     product = Product.new(title:       products(:ruby).title,
                           description: "yyy", 
