0f6800e5bac688137cd1661b45a25c5ca935c3ea
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-28 20:46:32 -0700

    Updated database to reflect the merged quantities and added ability to rollback.

--- project03/app/models/cart.rb
+++ project03/app/models/cart.rb
@@ -4,12 +4,12 @@ class Cart < ActiveRecord::Base
 	def add_product(product_id)
 		#Active Record knows to make a "Find_by" searching by
 		#the column name of "product_id"
-		current_item = lineitems.find_by_product_id(product_id)
-		if current_item
-			current_item.quantity += 1
-		else
-			current_item = line_items.build(product_id: product_id)
-		end
-		current_item
-	end
+    current_item = line_items.find_by_product_id(product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
 end
--- project03/db/schema.rb
+++ project03/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120128232630) do
+ActiveRecord::Schema.define(:version => 20120129033828) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
