d1c39deffe8433dd07c775ac154a4004ca00bac5
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-28 21:55:10 -0700

    Added a test for the javascript that was added for the additions to cart

--- project03/test/functional/line_items_controller_test.rb
+++ project03/test/functional/line_items_controller_test.rb
@@ -57,4 +57,10 @@ class LineItemsControllerTest < ActionController::TestCase
 			assert_select 'tr#current_item td', /Programming Ruby 1.9/
 		end
 	end
+	
+	test "markup needed for store.js.coffee is in place" do
+		get :index
+		assert_select '.store .entry > img', 3
+		assert_select '.entry input[type=submit]', 3
+	end
 end
