b05c74aa4c0f3fb943cbde74ad827aedab1ea3e3
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-28 21:04:34 -0700

    Added total to the cart.

--- project03/app/models/line_item.rb
+++ project03/app/models/line_item.rb
@@ -2,4 +2,7 @@ class LineItem < ActiveRecord::Base
 	#links the line item to the product and cart tables
 	belongs_to :product
 	belongs_to :cart
+	def total_price
+		product.price * quantity
+	end
 end
