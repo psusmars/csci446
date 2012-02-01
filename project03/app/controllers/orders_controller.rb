d11f7776a754db454e111d8cbd66791d9ced84f9
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-01 07:33:42 -0700

    Fixed the pagination problem, cant send it an array.

--- project03/app/controllers/orders_controller.rb
+++ project03/app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
+		@orders = Order.paginate page: params[:page], order: 'created_at desc',
+				per_page: 10
     @orders = Order.all
 
     respond_to do |format|
