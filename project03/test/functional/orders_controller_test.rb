7bb281f312a58b7c528ceafea451bf07b40b6a71
Paul Susmarski <psusmars@mymail.mines.edu>
2012-01-31 17:04:28 -0700

    Added tests for empty cart

--- project03/test/functional/orders_controller_test.rb
+++ project03/test/functional/orders_controller_test.rb
@@ -12,6 +12,10 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+		cart = Cart.create
+		session[:cart_id] = cart.id
+		LineItem.create(cart: cart, product: products(:ruby))
+		
     get :new
     assert_response :success
   end
@@ -46,4 +50,11 @@ class OrdersControllerTest < ActionController::TestCase
 
     assert_redirected_to orders_path
   end
+	
+	test "requires item in cart" do
+		get :new
+		assert_redirected_to store_path
+		assert_equal flash[:notice], 'Your cart is empty'
+	end
+	
 end
