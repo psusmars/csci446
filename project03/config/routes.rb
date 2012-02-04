1b488fcac3b45a283be9a4b75383b9c84b516bb3
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-03 21:15:54 -0700

    Added tests for admin login and updated the screen.

--- project03/config/routes.rb
+++ project03/config/routes.rb
@@ -1,12 +1,12 @@
 Depot::Application.routes.draw do
-  get "admin/index"
-
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
+	get 'admin' => 'admin#index'
+	
+	controller :sessions do
+		get 'login' => :new
+		post 'login' => :create
+		delete 'logout' => :destroy
+	end
+	
   resources :users
 
   resources :orders
