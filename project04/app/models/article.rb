bd19472a38b991864cbe786d51a76371237239e1
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-08 18:57:19 -0700

    Started working on the seed, messed up the schema, doing a rollback.

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+self.created_on.strftime("%B %d, %Y")
+
 end
--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120209012542) do
-
-  create_table "articles", :force => true do |t|
-    t.string   "title"
-    t.string   "author"
-    t.text     "body"
-    t.datetime "created_on"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
