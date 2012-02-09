01d5a05e776dc7f8066745648b8c854fa4ada25a
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-08 18:16:45 -0700

    Adding the skeleton of the rails application

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '10fda4b7dcd3517451e67046aae1e732baca6306124804780f01fb668280749e00250ce95905f347b3843ea9b6b8ded708d1751f1f1549fe2914e2fa4c83a489'
