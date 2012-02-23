532a9885d6f8cefdfba656475bf322ecc72f8907
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-23 16:18:11 -0700

    Generated the scaffolding for authors.

--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212231042) do
+ActiveRecord::Schema.define(:version => 20120223231446) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(:version => 20120212231042) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "edits",      :default => 0
+  end
+
+  create_table "authors", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
