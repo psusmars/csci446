4294e9db36a3ddcac7401a3098e449738b8c1330
Paul Susmarski <psusmars@mymail.mines.edu>
2012-02-23 16:23:30 -0700

    Completely forgot to add the name to the authors, added that.

--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120223231446) do
+ActiveRecord::Schema.define(:version => 20120223232020) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20120223231446) do
   create_table "authors", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
