60871fb50a7ff92ec3a4f06e850febb1bb0d4512
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-03 16:33:03 -0700

    Updated the index page for authors.

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -3,7 +3,10 @@ class Author < ActiveRecord::Base
 	
 	has_many :articles, dependent: :destroy
 	
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :photo, :styles => { :thumb => "100x100>" }
+	
+	validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/png', 'image/jpeg']
 	
 	validate :name_cant_contain_pat
 	
