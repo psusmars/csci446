ab88c484ed2369c8ab376a66e52a11b15cd77a24
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-21 22:10:40 -0600

    Adding initial gamez code Yeeeehaaawwww...

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '2d1be29b2f9fa8861ac3d18d819c89483ca68babd3acc1a546299e499ca86115cec8ce404fb7f7785054e8411cd106b2831231fe2027242d38a99d2e4981804f'
