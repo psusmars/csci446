aa2a3a42992019dda84451164c97f1f1fe106703
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-23 10:52:51 -0600

    Added validations for minimum lengths of passwords and usernames

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, uniqueness: true
+	validates :name, presence: true, uniqueness: true, minimum: 6
+	validates :password, minimum: 6
 	has_secure_password
 end
