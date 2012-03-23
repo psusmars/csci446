15f0d1051662eb1a6a555240833f03f18495ca3e
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-23 11:11:51 -0600

    Forgot a save to the previous commit

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true, minimum: 6
-	validates :password, minimum: 6
+	validates :password, minimum: 6 
 	has_secure_password
 end
