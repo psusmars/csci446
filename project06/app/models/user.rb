690fa1a2367a5042c68c804d983ce1cd97f05fce
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-24 09:35:13 -0600

    Added email to the user model, also cleaned up the views of the login page and register page.

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,22 +1,18 @@
 class User < ActiveRecord::Base
 	belongs_to :role
+	#has_one :profile
 	acts_as_authentic do |c|
 		c.validates_uniqueness_of_email_field_options = {:if => "false"}
 		c.validates_length_of_password_field_options = {:if => "true", :minimum => 6}
-		c.validates_format_of_login_field_options = {:if => "false", :with => Authlogic::Regex.login}
-		c.validates_length_of_email_field_options = {:if => "false", :minimum => 4}
 		c.validates_confirmation_of_password_field_options = {:if => "true"}
 		c.validates_length_of_password_confirmation_field_options = {:if => "false", :minimum => 4 }
 		c.validates_length_of_login_field_options = {:if => "true", :minimum => 6}
 	end
-	ROLES = %w[member admin]
-  
+	
   def role_symbols
     [role.to_sym]
   end
-	#validates :username, presence: true, uniqueness: true
-	#validates_length_of :password, minimum: 6
-	#validates_length_of :username, minimum: 6
-	#session[:user_id] = User.find().id
-	#@user = User.find(session[:user_id])
+	
+	
+	
 end
--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120324031358) do
+ActiveRecord::Schema.define(:version => 20120324151714) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20120324031358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
+    t.string   "email"
   end
 
 end
