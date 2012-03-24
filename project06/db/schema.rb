e3573ababfb18a5678946bc4a72ec1f0d81e9699
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-24 09:42:33 -0600

    Added profiles model and photos to profiles.

--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120324153825) do
+ActiveRecord::Schema.define(:version => 20120324153932) do
 
   create_table "profiles", :force => true do |t|
     t.string   "first_name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(:version => 20120324153825) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "roles", :force => true do |t|
