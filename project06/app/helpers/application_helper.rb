c3c318f76b4c53f4538970ad08beadfc5a8961c1
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-26 19:55:58 -0600

    Moved the darn helper methods to the user helpers as appropriate.

--- project06/app/helpers/application_helper.rb
+++ project06/app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-	def is_new_user_and_has_file? user
-		!@user.new_record? and @user.photo.file?
-	end
-
-	def get_first_and_last_name_string user
-		"#{user.first_name} #{user.last_name}"
-	end
-	
 end
--- project06/app/helpers/users_helper.rb
+++ project06/app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def is_new_user_and_has_file? user
+		!user.new_record? and user.photo.file?
+	end
+	
+	def get_first_and_last_name_string user
+		"#{user.first_name} #{user.last_name}"
+	end
+	
 end
