aa803c708405079f96d2ad40c3441e3d540a0934
Paul Susmarski <psusmars@mymail.mines.edu>
2012-03-29 10:06:40 -0600

    Removed unnecessary field for the session model.

--- project06/db/migrate/20120329125532_add_user_id_to_user_sessions.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToUserSessions < ActiveRecord::Migration
-  def change
-    add_column :user_sessions, :user_id, :integer
-  end
-end
--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329125532) do
+ActiveRecord::Schema.define(:version => 20120329125354) do
 
   create_table "games", :force => true do |t|
     t.string   "title"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20120329125532) do
   create_table "user_sessions", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
