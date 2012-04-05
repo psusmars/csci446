a0e22f9044381dd216b59886d618e35854174be9
Paul Susmarski <psusmars@mymail.mines.edu>
2012-04-05 06:52:35 -0600

    Added some eager loading to the user model to decrease DB load for users.

--- project06/app/models/game.rb
+++ project06/app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ActiveRecord::Base
 	belongs_to :user, :counter_cache => true
 	validates :title, presence: true
+	
+	default_scope :include => [:user]
+	
 	RATINGS = %w[Amazing Good Meh. Horrible]
 	def rating_symbols
     [RATINGS.to_sym]
