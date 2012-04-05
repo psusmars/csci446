649df30542326272abe89d83cded197c254c4706
Paul Susmarski <psusmars@mymail.mines.edu>
2012-04-04 21:45:26 -0600

    Removed uneccessary resources in routes.

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,22 +1,8 @@
 Gamez::Application.routes.draw do
   resources :games
-
-  resources :roles
-
   resources :user_sessions
-
   resources :users
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+	
 	controller :user_sessions do
 		get 'login' => :new
 		post 'login' => :create
