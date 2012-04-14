8f2958ddee2e3825c949e81c72b4681c154f37f5
Paul Susmarski <psusmars@mymail.mines.edu>
2012-04-14 10:58:16 -0600

    Added airplane text animation to when you win.

--- project07/guess_the_number/main.js
+++ project07/guess_the_number/main.js
@@ -32,7 +32,7 @@ function theGuess() {
 		var name = prompt("Enter your name for high scores: ", "Anonymous");
 		if( name != null ){ highScores.push([guessesLeft, name]); }
 		populateHighScores(highScores);
-		$('form#guessTheNumber').before("<p>Play again?</p>");
+		$("#end_game_text").airport([ "You're brains are Zombified!", "You have lost!", "Play again?" ]);
 	} else if (guess > answer) {
 		alert("Your guess is too high!");
 	} else {
@@ -61,3 +61,41 @@ function SortByScore(x, y) {
 function generateNumberToGuess() {
 	answer = Math.floor((Math.random()*100)+1);
 }
+
+
+$.fn.extend({
+    airport: function(array) {
+        var self = $(this);
+        //only modified this array
+        //added the needed characters
+        var chars = ['P', '\'', 'Z', 'Y', 'a', 'b', 'c', 'd', 'e', 'f', 'g', ' ', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '!', '?'];        var longest = 0;
+        var items = items2 = array.length;
+
+        function pad(a, b) { return a + new Array(b - a.length + 1).join(' '); }
+        $(this).empty();
+        while (items--)
+            if (array[items].length > longest) longest = array[items].length;
+        while (items2--)
+            array[items2] = pad(array[items2], longest);
+        spans = longest;
+        while (spans--)
+            $(this).prepend("<span class='c" + spans + "'></span>");
+        function testChar(a, b, c, d) {
+            if (c >= array.length) setTimeout(function() {
+                testChar(0, 0, 0, 0);
+            }, 1000);
+            else if (d >= longest) setTimeout(function() {
+                testChar(0, 0, c + 1, 0);
+            }, 1000);
+            else {
+                $(self).find('.c' + a).html((chars[b] == " ") ? "&nbsp;" : chars[b]);
+                setTimeout(function() {
+                    if (b > chars.length) testChar(a + 1, 0, c, d + 1);
+                    else if (chars[b] != array[c].substring(d, d + 1).toLowerCase()) testChar(a, b + 1, c, d);
+                    else testChar(a + 1, 0, c, d + 1);
+                }, 20);
+            }
+        }
+        testChar(0, 0, 0, 0);
+    }
+});
